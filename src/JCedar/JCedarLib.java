755f817e7fc02c54305b40b6b1ebee6f1a20c710
BT_1N3_07 <bsmith3816@bths.edu>
2018-09-20 11:33:12 -0400

    Added 3 new functions and tested them

--- src/JCedar/JCedarLib.java
+++ src/JCedar/JCedarLib.java
@@ -1,6 +1,9 @@
 package JCedar;
 
 public class JCedarLib {
+
+    //Method creates an inverted string from the input string and compares the strings to eachother
+    //Method Finds if the string is a palindrome
     public static boolean isPalindrome(String str){
         String y = "";
         for(int i = 0; i < str.length(); i++){
@@ -20,5 +23,34 @@ public class JCedarLib {
         }
 
     }
-    
+    //Pulls apart the input string into pieces and recreates the string with the new information
+    public static String dateStr(String str){
+        String x = str.substring(0,2);
+        String y = str.substring(3,5);
+        String z = str.substring(6, str.length());
+        System.out.println(y + "-" + x + "-" + z);
+        return y + "-" + x + "-" + z;
+    }
+    //Method starts at zero and adds up to the initial value similar to a factorial
+    public static int sumUpTo(int n){
+        int x = 0;
+        for(int i = 0; i < n+1; i++){
+            x = x + i;
+        }
+        System.out.println(x);
+        return x;
+    }
+    //Method takes a starting number and multiplies it up to the range value and gives all the results
+    public static void multiplacationTable(int base, int range){
+        String table = "";
+        for(int i = 0; i <= range; i++){
+            int y = base * i;
+            table = table + y + ",";
+
+        }
+        System.out.println(table);
+    }
+    public static String cutOut(String mainStr, String subStr){
+
+    }
 }
--- src/JCedar/Main.java
+++ src/JCedar/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	JCedarLib.isPalindrome("no");
-
+    JCedarLib.sumUpTo(4);
+    JCedarLib.multiplacationTable(2, 6);
     }
 }
